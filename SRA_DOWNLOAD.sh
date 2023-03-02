3dc162d24b56e68c694ef08adf9ccdb4152e754d
MaxLeachBioinformatics <125990052+MaxLeachBioinformatics@users.noreply.github.com>
2023-03-02 13:35:04 +0000

    Add files via upload
    Shell Script for installation. Ascension list is the list of each cell it will download from.
    
    Important notes:
    -First of all you will need to edit the email at the end of the script so it is YOUR email. I have it set to a dummy variable for now
    -Secondly, make sure you configure the SRA toolkit module on SPECTRE
    -Thirdly, try and run the script with an abridged test list first to ensure it all works

---
+++ SRA_DOWNLOAD.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#:oad Modules
+module load sratoolkit/2.11.1
+
+# Navigate to the directory containing the script
+cd "$(dirname "$0")"
+
+
+
+# Read accession numbers from the file and store in an array
+mapfile -t ACCESSIONS < Ascension_list.txt
+
+cd $SCRATCHDIR
+
+# Create a new directory for storing downloaded files
+mkdir -p sradata
+
+cd sradata
+mkdir -p fastqdump
+# Loop through each accession number and download the corresponding file
+for ACCESSION in "${ACCESSIONS[@]}"
+do
+  echo "Downloading file for accession: $ACCESSION"
+  prefetch $ACCESSION
+  fasterq-dump $ACCESSION -O ./fastqdump/
+done
+echo "SRA download complete" | mail -s "SRA download complete" your_email@example.com
